912a4521a09c3bd34bb74d43a3e1df877d4a9a0a
Mini256 <minianter@foxmail.com>
2024-02-29 23:29:20 +0800

    fix ci

--- src/vector.ts
+++ src/vector.ts
@@ -2,39 +2,51 @@ import { ExpressionBuilder, ReferenceExpression } from 'kysely';
 
 type VectorLike = Float32Array | number[];
 
-export function l1Distance<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
-  return eb.fn<number>('VEC_L1_DISTANCE', [
-    column,
-    eb => eb.val(vectorToSql(value))],
-  );
+export function l1Distance<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(
+  eb: ExpressionBuilder<DB, TB>,
+  column: RE,
+  value: VectorLike
+) {
+  return eb.fn<number>('VEC_L1_DISTANCE', [column, (eb) => eb.val(vectorToSql(value))]);
 }
 
-export function l2Distance<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
-  return eb.fn<number>('VEC_L2_DISTANCE', [
-    column,
-    eb => eb.val(vectorToSql(value))],
-  );
+export function l2Distance<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(
+  eb: ExpressionBuilder<DB, TB>,
+  column: RE,
+  value: VectorLike
+) {
+  return eb.fn<number>('VEC_L2_DISTANCE', [column, (eb) => eb.val(vectorToSql(value))]);
 }
 
-export function negativeInnerProduct<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
-  return eb.fn<number>('VEC_NEGATIVE_INNER_PRODUCT', [
-    column,
-    eb => eb.val(vectorToSql(value))],
-  );
+export function negativeInnerProduct<
+  DB,
+  TB extends keyof DB,
+  RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>
+>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
+  return eb.fn<number>('VEC_NEGATIVE_INNER_PRODUCT', [column, (eb) => eb.val(vectorToSql(value))]);
 }
 
-export function innerProduct<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
+export function innerProduct<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(
+  eb: ExpressionBuilder<DB, TB>,
+  column: RE,
+  value: VectorLike
+) {
   return eb.neg(negativeInnerProduct(eb, column, value));
 }
 
-export function cosineDistance<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
-  return  eb.fn<number>('VEC_COSINE_DISTANCE', [
-    column,
-    eb => eb.val(vectorToSql(value))],
-  );
+export function cosineDistance<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(
+  eb: ExpressionBuilder<DB, TB>,
+  column: RE,
+  value: VectorLike
+) {
+  return eb.fn<number>('VEC_COSINE_DISTANCE', [column, (eb) => eb.val(vectorToSql(value))]);
 }
 
-export function cosineSimilarity<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
+export function cosineSimilarity<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(
+  eb: ExpressionBuilder<DB, TB>,
+  column: RE,
+  value: VectorLike
+) {
   return eb(eb.lit<number>(1), '-', cosineDistance(eb, column, value));
 }
 
