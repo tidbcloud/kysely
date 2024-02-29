1a8e8dce117daa47ab88bef10fdd30ead07d1000
Mini256 <minianter@foxmail.com>
2024-02-29 23:27:13 +0800

    more type-safe impl

--- src/vector.ts
+++ src/vector.ts
@@ -1,29 +1,41 @@
-import { RawBuilder, sql } from 'kysely';
+import { ExpressionBuilder, ReferenceExpression } from 'kysely';
 
 type VectorLike = Float32Array | number[];
 
-export function l1Distance(column: string, value: VectorLike): RawBuilder<number> {
-  return sql`VEC_L1_DISTANCE(${sql.ref(column)}, ${vectorToSql(value)})`;
+export function l1Distance<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
+  return eb.fn<number>('VEC_L1_DISTANCE', [
+    column,
+    eb => eb.val(vectorToSql(value))],
+  );
 }
 
-export function l2Distance(column: string, value: VectorLike): RawBuilder<number> {
-  return sql`VEC_L2_DISTANCE(${sql.ref(column)}, ${vectorToSql(value)})`;
+export function l2Distance<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
+  return eb.fn<number>('VEC_L2_DISTANCE', [
+    column,
+    eb => eb.val(vectorToSql(value))],
+  );
 }
 
-export function negativeInnerProduct(column: string, value: VectorLike): RawBuilder<number> {
-  return sql`VEC_NEGATIVE_INNER_PRODUCT(${sql.ref(column)}, ${vectorToSql(value)})`;
+export function negativeInnerProduct<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
+  return eb.fn<number>('VEC_NEGATIVE_INNER_PRODUCT', [
+    column,
+    eb => eb.val(vectorToSql(value))],
+  );
 }
 
-export function innerProduct(column: string, value: VectorLike): RawBuilder<number> {
-  return sql`1 - VEC_NEGATIVE_INNER_PRODUCT(${sql.ref(column)}, ${vectorToSql(value)})`;
+export function innerProduct<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
+  return eb.neg(negativeInnerProduct(eb, column, value));
 }
 
-export function cosineDistance(column: string, value: VectorLike): RawBuilder<number> {
-  return sql`VEC_COSINE_DISTANCE(${sql.ref(column)}, ${vectorToSql(value)})`;
+export function cosineDistance<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
+  return  eb.fn<number>('VEC_COSINE_DISTANCE', [
+    column,
+    eb => eb.val(vectorToSql(value))],
+  );
 }
 
-export function cosineSimilarity(column: any, value: VectorLike): RawBuilder<number> {
-  return sql`1 - VEC_COSINE_DISTANCE(${sql.ref(column)}, ${vectorToSql(value)})`;
+export function cosineSimilarity<DB, TB extends keyof DB, RE extends ReferenceExpression<DB, TB> = ReferenceExpression<DB, TB>>(eb: ExpressionBuilder<DB, TB>, column: RE, value: VectorLike) {
+  return eb(eb.lit<number>(1), '-', cosineDistance(eb, column, value));
 }
 
 export function vectorFromSql(value: string) {
